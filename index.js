b59565c35e5840efe637b81720176996d810e321
Meredith Jones <mj19003@gmail.com>
2021-01-08 09:22:40 -0500

    update with sql connection setup

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const config = require('./package.json');
+
+
+// Setting up the connection to mysql
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    // Your MySQL username
+    user: 'root',
+    // Your MySQL password 
+    password: '',
+    // Name of database
+    database: "employee_tracker_db",
+  });
+  
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    init();
+  });
+  
+  //Start the inquirer prompts
